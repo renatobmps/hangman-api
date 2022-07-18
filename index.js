f134edf0c6475e051f0b7b7792add59c8e537a1f
Renato Brandão <renatobmps.dev@gmail.com>
2022-07-18 02:18:09 -0300

    [FIX] port

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port ${process.env.PORT || 3000}`);
 });
 
 // middlewares
