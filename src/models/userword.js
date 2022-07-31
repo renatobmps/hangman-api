91b420c28e39e9c7ce060e904dd59b96525864fa
Renato Brandão <renatobmps.dev@gmail.com>
2022-07-31 18:13:51 -0300

    [FEAT] adding postgres

--- src/models/userword.js
+++ src/models/userword.js
@@ -4,11 +4,6 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class UserWord extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       UserWord.belongsTo(models.User, {
         foreignKey: 'idUsers',
