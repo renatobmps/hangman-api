82a2dd4bc03192049ad3b8040957591c9b426aab
Renato Brandão <renatobmpsilva@gmail.com>
2022-09-12 10:59:39 -0300

    [FIX] trying resolve CORS

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors('*'));
+app.use(cors('https://forca-renatobmps.vercel.app'));
 
 db.sequelize.sync().then(() => {
   app.listen(process.env.PORT || 3000, () => {
